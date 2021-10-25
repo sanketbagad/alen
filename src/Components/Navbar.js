fc292625ed59fbe7f0b05dd86a23613ed06d1261
Sanket Bagad <alencolins@gmail.com>
2021-10-26 01:02:40 +0530

    structuring and styling the navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,10 @@ const Navbar = () => {
         <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
             <div className="flex justify-between items-center space-x-5 w-screen">
             <Link to="/">
-                <p className="text-2xl"> Alen </p>
+                <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900"> Alen ❗❗ </p>
+                <button type="button" className="" onClick={() =>}>
+
+                </button>
             </Link>
             </div>
         </div>
