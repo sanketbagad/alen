710cf04a1dffc2e40b552321459c40e13c10369f
Sanket Bagad <alencolins@gmail.com>
2021-10-26 00:54:55 +0530

    Some bugs :(

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
             <div className="flex justify-between items-center space-x-5 w-screen">
-                
+            <Link to="/">
+                <p className="text-2xl"> Alen </p>
+            </Link>
             </div>
         </div>
     )
