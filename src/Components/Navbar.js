5bae907d0565a3f151947aa6d7148a33a665dc2f
Sanket Bagad <alencolins@gmail.com>
2021-10-26 00:47:11 +0530

    Working on Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
+            <div className="flex justify-between items-center space-x-5 w-screen">
+                
+            </div>
         </div>
     )
 }
