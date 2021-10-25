2802cd0393f3ef04ee6e18905b57d6fcb1c8078d
Sanket Bagad <alencolins@gmail.com>
2021-10-26 00:42:27 +0530

    Some setup and going through tailwind-css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import React, {useState} from 'react'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
+import Routes from './Components/Routes'
 
 function App() {
+  const [darkTheme, setDarkTheme] = useState(false)
   return (
-    <div>
-     hELLO wORLD
+    <div className={darkTheme ? "dark" : ""}>
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+        <Navbar />
+        <Routes />
+        <Footer />
+      </div>
     </div>
   );
 }
