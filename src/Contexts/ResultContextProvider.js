4dfd8e312e5989ccee4d4ca32809385b813d5538
Sanket Bagad <alencolins@gmail.com>
2021-11-02 23:45:36 +0530

    Completing the context provider

--- src/Contexts/ResultContextProvider.js
+++ src/Contexts/ResultContextProvider.js
@@ -7,4 +7,32 @@ const BaseUrl = 'https://google-search3.p.rapidapi.com/api/v1'
 export const ResultContextProvider = ({ children }) => {
     const [result, setResults] = useState([])
     const [isLoading, setIsLoading] = useState(false)
+    const [searchTerm, setSearchTerm] = useState("")
+
+    const getResults = async (type) => {
+        setIsLoading(true)
+
+        const response = await fetch(`${BaseUrl}${type}`, {
+            method: "GET",
+            headers: {
+                'x-user-agent': 'desktop',
+                'x-proxy-location': 'US',
+                'x-rapidapi-host': 'google-search3.p.rapidapi.com',
+                'x-rapidapi-key': 'fc2d932218msh64f21ad4212ecd3p1af359jsn1c8c5abd9df9'
+              }
+        })
+
+        const data = await response.json()
+
+        setResults(data)
+        setIsLoading(false)
+    }
+
+    return (
+        <ResultContext.Provider value={{ getResults, result, searchTerm, setSearchTerm, isLoading}}>
+            {children}
+        </ResultContext.Provider>
+    )
 }
+
+export const useResultContext = () => useContext(ResultContext)
