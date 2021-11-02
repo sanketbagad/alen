96129f0a40318c085046446ba91502acc1c6123a
Sanket Bagad <alencolins@gmail.com>
2021-11-02 23:09:13 +0530

    Working on the Routes Component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,8 +7,8 @@ const Navbar = ({setDarkTheme, darkTheme}) => {
             <div className="flex justify-between items-center space-x-5 w-screen">
             <Link to="/">
                 <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900"> Alen ❗❗ </p>
-                <button type="button" className="" onClick={() => setDarkTheme(!darkTheme)}>
-
+                <button type="button" className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg " onClick={() => setDarkTheme(!darkTheme)}>
+                    {darkTheme ? "Light": "Dark"}
                 </button>
             </Link>
             </div>
--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Switch, Route, Redirect} from 'react-router-dom'
 
 const Routes = () => {
     return (
