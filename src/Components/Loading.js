da2e31ec2670e56dde50982f0450be686c81fdd6
Sanket Bagad <alencolins@gmail.com>
2021-11-02 23:53:30 +0530

    Working on the Loader component

---
+++ src/Components/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Loader from "react-loader-spinner"
+const Loading = () => {
+    return (
+        <div>
+            Loading
+        </div>
+    )
+}
+
+export default Loading
