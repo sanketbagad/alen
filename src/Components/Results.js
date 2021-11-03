56c651136b510789df7004dc7d9853a1bc7d298f
Sanket Bagad <alencolins@gmail.com>
2021-11-03 14:36:44 +0530

    Running into some bugs while fetching news

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -6,12 +6,19 @@ import { useResultContext } from '../Contexts/ResultContextProvider'
 import Loading from './Loading'
 
 const Results = () => {
-    const { result, isLoading, getResults, searchTerm} = useResultContext()
+    const { result: {results, image_results, entries: news}, isLoading, getResults, searchTerm} = useResultContext()
     const location = useLocations()
 
     useEffect(() => {
-        getResults("/search/q=Sanket Bagad&num=48")
-    },[])
+        if(searchTerm) {
+            if(location.pathname === "videos") {
+                getResults(`/search/q=${searchTerm} videos`)
+            } else {
+                getResults(`${location.pathname}/${searchTerm}&num=60`)
+            }
+        }
+        
+    },[searchTerm, location.pathname])
 
     if(isLoading) return <Loading />
 
@@ -19,9 +26,9 @@ const Results = () => {
        case "/search":
            return (
                <div className="flex flex-wrap justify-between space-y-6 sm:px-56 ">
-                   {result?.results?.map(({ link, title}, index) => (
+                   {results?.map(({ link, title}, index) => (
                        <div key={index} className="md:w-2/5 w-full" >
-                           <a href={link} target="_blank" ref={="noreferrer"}>
+                           <a href={link} target="_blank" rel="norefferer noreferrer">
                                 <p className="text-sm">
                                     {link.length > 30 ? link.substring(0, 30) : link}
                                 </p>
@@ -37,14 +44,36 @@ const Results = () => {
         case "/images":
             return (
                 <div className="flex flex-wrap justify-center items-center">
-                    {result?.image_results?.map(({ image, link: { href, title}}, index) => (
-
+                    {image_results?.map(({ image, link: { href, title}}, index) => (
+                        <a className="sm:p-3 p-5" href={href} key={index} target="_blank" rel="norefferer noreferrer">
+                            <img className="" src={image?.src} alt={title} loading="lazy" />
+                            <p className="text-sm w-36 break-words mt-2">
+                                {title}
+                            </p>
+                        </a>
                     ))}
                 </div>
             )
 
         case "/news":
-            return ""
+            return (
+                <div className="flex flex-wrap items-center justify-between space-y-6 sm:px-56 ">
+                    {news?.map(({ links, title, source, id}) => (
+                        <div key={id} className="md:w-2/5 w-full" >
+                            <a href={links?.[0].href} target="_blank" rel="norefferer noreferrer" className="hover:underline">
+                                 <p className="text-lg dark:text-blue-300 text-blue-700">
+                                     {title}
+                                 </p>
+                            <div className="flex gap-4">
+                                <a href={source?.href} target="_blank" rel="norefferer noreferrer" className="hover:underline">
+                                    {source?.href}
+                                </a>
+                            </div>
+                            </a>
+                        </div>
+                    ))} 
+                </div>
+            )
 
         case "/videos":
             return ""
--- src/Contexts/ResultContextProvider.js
+++ src/Contexts/ResultContextProvider.js
@@ -24,6 +24,14 @@ export const ResultContextProvider = ({ children }) => {
 
         const data = await response.json()
 
+        if(type.includes('/news')) {
+            setResults( data.entries)
+        } else if(type.includes("/images")) {
+             setResults(data.image_results)
+        } else {
+            setResults(data.results)
+        }
+
         setResults(data)
         setIsLoading(false)
     }
