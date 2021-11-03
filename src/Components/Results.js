3b1c6f1f3eb68f3ddacf562aab1cc7de92ca9ffa
Sanket Bagad <alencolins@gmail.com>
2021-11-03 14:04:03 +0530

    Working on image results

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -9,18 +9,39 @@ const Results = () => {
     const { result, isLoading, getResults, searchTerm} = useResultContext()
     const location = useLocations()
 
+    useEffect(() => {
+        getResults("/search/q=Sanket Bagad&num=48")
+    },[])
+
     if(isLoading) return <Loading />
 
    switch (location.pathname) {
        case "/search":
            return (
                <div className="flex flex-wrap justify-between space-y-6 sm:px-56 ">
-                    
+                   {result?.results?.map(({ link, title}, index) => (
+                       <div key={index} className="md:w-2/5 w-full" >
+                           <a href={link} target="_blank" ref={="noreferrer"}>
+                                <p className="text-sm">
+                                    {link.length > 30 ? link.substring(0, 30) : link}
+                                </p>
+                                <p className="text-lg hover:underline dark:text-blue-300 text-blue-700">
+                                    {title}
+                                </p>
+                           </a>
+                       </div>
+                   ))} 
                </div>
            )
         
         case "/images":
-            return ""
+            return (
+                <div className="flex flex-wrap justify-center items-center">
+                    {result?.image_results?.map(({ image, link: { href, title}}, index) => (
+
+                    ))}
+                </div>
+            )
 
         case "/news":
             return ""
